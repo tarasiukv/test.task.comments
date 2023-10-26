738950c0ede21486f1c0c2453e1de4760b58a015
ii <i.broshchak@gmail.com>
2023-10-26 12:51:55 +0300

    Add base migrations

---
+++ database/migrations/2023_10_26_094248_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+  use Illuminate\Database\Migrations\Migration;
+  use Illuminate\Database\Schema\Blueprint;
+  use Illuminate\Support\Facades\Schema;
+
+  return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::create('comments', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')
+          ->constrained('users')
+          ->onUpdate('cascade')
+          ->onDelete('cascade');
+        $table->foreignId('comment_id')
+          ->nullable()
+          ->constrained('comments')
+          ->onUpdate('cascade')
+          ->nullOnDelete();
+        $table->text('text')->nullable();
+        $table->string('image_path')->nullable();
+        $table->string('text_file_path')->nullable();
+        $table->timestamps();
+
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+      Schema::dropIfExists('comments');
+    }
+  };
