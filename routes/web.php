00af26d508ffa8872a46ecb0605c0e60cbbcc077
ii <i.broshchak@gmail.com>
2023-10-26 12:23:08 +0300

    Base web router

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+  use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+  /*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+  Route::get('/{any}', function () {
+    return view('layouts.app');
+  })->where('any', '.*');
