8549645f06934e598f07dc9b6a3ae596f2532314
ii <i.broshchak@gmail.com>
2023-10-27 23:23:40 +0300

    Update component tree

--- resources/js/components/ChildCommentComponent.vue
+++ resources/js/components/ChildCommentComponent.vue
@@ -36,20 +36,27 @@ const addReply = (comment) => {
 
 <template>
 
-    <ul>
-      <li v-for="reply in props.replies" :key="reply.id">
-        <div>
-          <strong>{{ reply.id }}</strong> - {{ reply.email }}
+        <div v-for="reply in props.replies" :key="reply.id" class="media-block" >
+            <a class="media-left" href="#"><img class="img-circle img-sm" alt="Profile Picture" src="https://bootdey.com/img/Content/avatar/avatar2.png"></a>
+            <div class="media-body">
+                <div class="mar-btm">
+                    <a href="#" class="btn-link text-semibold media-heading box-inline">{{ reply.email }}</a>
+                    <p class="text-muted text-sm"><i class="fa fa-mobile fa-lg"></i>{{ reply.created_at }}</p>
+                </div>
+                <p>{{ reply.text }}</p>
+                <div class="pad-ver">
+                    <div class="btn-group">
+                    </div>
+                    <div v-if="reply.replying">
+                        <input v-model="reply.reply_name" placeholder="Your Name" />
+                        <input v-model="reply.reply_email" placeholder="Your Email" />
+                        <textarea v-model="reply.reply_text" placeholder="Your Reply"></textarea>
+                        <button @click="addReply(reply)">Add Reply</button>
+                    </div>
+                    <button class="btn btn-sm btn-default btn-hover-primary" @click="addReply(reply)">Comment</button>
+                </div>
+                <hr>
+                <child-comment-component :replies="reply.descendants"></child-comment-component>
+            </div>
         </div>
-        <div>{{ reply.text }}</div>
-        <button @click="replyToComment(reply)">Reply</button>
-        <div v-if="reply.replying">
-          <input v-model="reply.reply_name" placeholder="Your Name" />
-          <input v-model="reply.reply_email" placeholder="Your Email" />
-          <textarea v-model="reply.reply_text" placeholder="Your Reply"></textarea>
-          <button @click="addReply(reply)">Add Reply</button>
-        </div>
-        <child-comment-component :replies="reply.descendants"></child-comment-component>
-      </li>
-    </ul>
 </template>
--- resources/js/components/CommentsComponent.vue
+++ resources/js/components/CommentsComponent.vue
@@ -57,32 +57,7 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div id="wrapper">
-        <h2>Comments</h2>
-
-        <div>
-            <h3>Add a New Comment</h3>
-            <form @submit.prevent="addNewComment">
-                <div>
-                    <p>Enter <input v-model="newComment.name" placeholder="Your Name" required/></p>
-                </div>
-                <div>
-                    <p>Enter <input v-model="newComment.email" placeholder="Your Email" required/></p>
-                </div>
-                <div>
-                    <h5>Enter your comment</h5>
-                    <textarea v-model="newComment.text" placeholder="Your Comment" required></textarea>
-                </div>
-                <div>
-                    <button type="submit">Add Comment</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-
     <div class="container mb-5 mt-5">
-
         <div class="card">
             <div class="row">
                 <div class="col-md-12">
@@ -122,6 +97,79 @@ onMounted(async () => {
                                         <child-comment-component :replies="comment.descendants" />
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="container bootdey">
+        <div class="col-md-12 bootstrap snippets">
+            <div class="panel">
+                <div class="panel-body">
+                    <h3>Add a New Comment</h3>
+                    <form @submit.prevent="addNewComment">
+                        <div>
+                            <p>Enter <input v-model="newComment.name" class="form-control"  placeholder="Your Name" required/></p>
+                        </div>
+                        <div>
+                            <p>Enter <input v-model="newComment.email" class="form-control"  placeholder="Your Email" required/></p>
+                        </div>
+                        <textarea
+                            v-model="newComment.text"
+                            class="form-control"
+                            rows="2"
+                            placeholder="What are you thinking?"
+                            required
+                        ></textarea>
+                        <div class="mar-top clearfix">
+                            <button class="btn btn-sm btn-primary pull-right" type="submit"><i class="fa fa-pencil fa-fw"></i> Share</button>
+                            <a class="btn btn-trans btn-icon fa fa-camera add-tooltip" href="#"></a>
+                            <a class="btn btn-trans btn-icon fa fa-file add-tooltip" href="#"></a>
+                        </div>
+                        <div>
+                            <button type="submit">Add Comment</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="panel">
+                <div
+                    class="panel-body"
+                    v-for="comment in comments" :key="comment.id"
+                >
+                    <div class="media-block">
+                        <a class="media-left" href="#"><img class="img-circle img-sm" alt="Profile Picture" src="https://bootdey.com/img/Content/avatar/avatar1.png"></a>
+                        <div class="media-body">
+                            <div class="mar-btm">
+                                <a href="#" class="btn-link text-semibold media-heading box-inline"> {{ comment.user.email }}</a>
+                                <p class="text-muted text-sm"> {{ comment.created_at }}</p>
+                            </div>
+                            <p>{{ comment.text }}</p>
+<!--                            For images or files    -->
+<!--                            <img class="img-responsive thumbnail" src="" alt="Image">-->
+                            <div class="pad-ver">
+                                <button class="btn btn-sm btn-default btn-hover-primary" @click="replyToComment(comment)"><i class="fa fa-reply"></i> Comment</button>
+                                <button @click="toggleChildComments(comment.id)" class="btn btn-sm btn-default btn-hover-primary">
+                                    {{ showComments[comment.id] ? 'Hide' : 'Show' }} comments
+                                </button>
+                            </div>
+
+                            <div v-if="comment.replying">
+                                <div><input v-model="comment.reply_name" placeholder="Your Name" /></div>
+                                <input v-model="comment.reply_email" placeholder="Your Email" />
+                                <textarea v-model="comment.reply_text" class="form-control" placeholder="Your Reply" required></textarea>
+                                <button @click="addReply(comment)">Add Reply</button>
+                            </div>
+
+                            <hr>
+                            <div v-if="showComments[comment.id]">
+                                <child-comment-component :replies="comment.descendants" />
                             </div>
                         </div>
                     </div>
