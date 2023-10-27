768860c0608a8adaea282cf789ca7cb5d5eef0d3
ii <i.broshchak@gmail.com>
2023-10-27 22:58:31 +0300

    Update Comment resource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,6 +22,7 @@ class CommentResource extends JsonResource
           'text' => $this->text,
           'image_path' => $this->image_path,
           'text_file_path' => $this->text_file_path,
+          'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
           'user' => new UserResource($this->whenLoaded('user')),
 //          'childComments' => CommentResource::collection($this->whenLoaded('childComments')),
 //          'parentComment' => new CommentResource($this->whenLoaded('parentComment')),
