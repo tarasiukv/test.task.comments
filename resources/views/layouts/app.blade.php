163df26e80b2e8322c189d2cf2724ed42d81fc3b
ii <i.broshchak@gmail.com>
2023-10-27 20:36:13 +0300

    Add base style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,14 @@
     <link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- Favicon-->
 {{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>--}}
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     @vite('resources/css/app.css')
 </head>
 <body class="hold-transition dark-skin theme-primary sidebar-mini fixed">
